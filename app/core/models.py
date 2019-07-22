d989661905bc7c870906f8bbbfb04855348dbeff
satish04121995 <satishkrgu95@gmail.com>
2019-07-23 01:09:26 +0530

    fixes

--- app/core/models.py
+++ app/core/models.py
@@ -7,9 +7,9 @@ class UserManager(BaseUserManager):
 
 
 	def create_user(self, email, password=None, **extra_fields):
-		""" CReate nd save the new user """
+		"""CReate nd save the new user """
 		if not email:
-			raise ValueError(" user must have an email address")
+			raise ValueError("user must have an email address")
 		user = self.model(email=self.normalize_email(email), **extra_fields)
 		user.set_password(password)
 		user.save(using= self._db)
@@ -17,7 +17,7 @@ class UserManager(BaseUserManager):
 		return user
 
 	def create_superuser(self, email, password):
-		""" Create a suoer user"""
+		"""Create a suoer user"""
 		user = self.create_user(email, password)
 		user.is_staff = True
 		user.is_superuser = True
@@ -28,10 +28,10 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
 	""" custo user model that support using email insted of user name"""
-	email = models.EmailField(max_length= 255, unique= True)
-	name = models.CharField(max_length= 255)
-	is_active = models.BooleanField(default= True)
-	is_staff = models.BooleanField(default= False)
+	email = models.EmailField(max_length=255, unique=True)
+	name = models.CharField(max_length=255)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
 
 	objects = UserManager()
 
